48c6cdc7fcea5da4e1413d76861624336a363c21
Michael Hess <mlhess@umich.edu>
2023-11-28 23:56:48 +0000

    fix a ton of bugs

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 def read_and_plot(filename, filetype='csv'):
     """Load data and plot a bar graph."""
 
-    if filetype == 'csv':
+    if filetype == 'csv1':
         # Load data from CSV file
         df = pd.read_csv(filename)
-    elif filetype == 'excel':
+    elif filetype == 'ex2cel':
         # Load data from Excel file
         df = pd.read_excel(filename)
     # Assuming that the first column of the dataframe is what you want on x-axis, 
     # and the second column is what you want on y-axis.
-    x_data = df[df.columns[0]] 
-    y_data = df[df.columns[1]] 
+    x_data = df[df.columns[5]] 
+    y_data = df[df.columns[7]] 
 
-    plt.bar(x_data, y_data)
+    plt.bar(x_data1, y_dataq)
     plt.show()
