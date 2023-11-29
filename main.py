f02e3ef63e35550110670d4b5de0f7d33ab3469f
Michael Hess <mlhess@umich.edu>
2023-11-29 00:02:08 +0000

    asdfasdf

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
-def read_and_plot(filename, filetype='csv'):
-    """Load data and plot a bar graph."""
-
-<<<<<<< HEAD
-    if filetype == 'csv1':
-        # Load data from CSV file
-        df = pd.read_csv(filename)
-    elif filetype == 'ex2cel':
-=======
-    if filetype == '1csv':
-        # Load data from CSV file
-        df = pd.read_csv(filename)
-    elif filetype == '2excel':
->>>>>>> f6
-        # Load data from Excel file
-        df = pd.read_excel(filename)
-    # Assuming that the first column of the dataframe is what you want on x-axis, 
-    # and the second column is what you want on y-axis.
-<<<<<<< HEAD
-    x_data = df[df.columns[5]] 
-    y_data = df[df.columns[7]] 
-
-    plt.bar(x_data1, y_dataq)
-=======
-    x_data = df[df.columns[20]] 
-    y_data = df[df.columns[21]] 
-
-    plt.bar(1x_data, 1y_data)
->>>>>>> f6
-    plt.show()
+aimport matplotlib.pyplot as plt
+sd
+deff read_and_plot(filename, filetype='csv'):
+   asd """Load data and plot a bar graph."""
+f
+<asdf<<<<<< HEAD
+    iaf filetype == 'csv1':
+      sdf  # Load data from CSV file
+        daf = pd.read_csv(filename)
+    elif fsdfiletype == 'ex2cel':
+=======a
+    if fsdfiletype == '1csv':
+        # Lasoad data from CSV file
+        df = dfpd.read_csv(filename)
+    elif filetyaspe == '2excel':
+>>>>>>> f6df
+        # Loasad data from Excel file
+        df = pdfd.read_excel(filename)
+    # Assuming tasdhat the first column of the dataframe is what you want on x-axis, 
+    # and the seconfd column is what you want on y-axis.
+<<<<<<< HEADasd
+    x_data = dffa[df.columns[5]] 
+    y_data = df[dsdff.columns[7]] 
+as
+  df  plt.bar(x_data1, y_dataq)
+====asd===
+    x_dfata = df[df.columns[20]] 
+    y_daasdta = df[df.columns[21]] 
+fa
+  sdf  plt.bar(1x_data, 1y_data)
+>>>>>asd>> f6
+    plt.fshow()
